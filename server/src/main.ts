17c1467d8a214e674aefbea292b6b8a50f13bfe7
adebowale damilola <damilola.adebowale@abbaandking.com>
2025-11-18 13:44:13 +0100

    fixes

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin:
-      'https://ai-resume-builder-p0xd2ufge-temitayo40s-projects.vercel.app/',
+      'https://ai-resume-builder-p0xd2ufge-temitayo40s-projects.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
